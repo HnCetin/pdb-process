06c7695b2ffe46df3003bc1dd12bd0c5c8b44671
HnCetin <36331653+HnCetin@users.noreply.github.com>
2019-03-19 03:10:44 +0300

    Add files via upload

--- Script.m
+++ Script.m
@@ -1,29 +1,39 @@
 %% Protein Selection
-% 4AKE 
-pro1 = getpdb('4AKE');
-pro1 = process_pdb(pro1, 'A');
+% pro1 = getpdb('4AKE');
+% pro2 = getpdb('1AKE');
+% save proteins.mat pro1 pro2
+load proteins.mat
 
-% 1AKE 
-pro2 = getpdb('1AKE');
+pro1 = process_pdb(pro1, 'A');
 pro2 = process_pdb(pro2, 'A');
+
 
 %% Plots 
 % Plots of bond properties:
 figure(1)
-    subplot(1,2,1);
+    subplot(2,2,1);
         plot3(pro1.ReducedModel.XYZ(:,1), pro1.ReducedModel.XYZ(:,2), pro1.ReducedModel.XYZ(:,3) )
         grid on
         title('4AKE in 3D Space')
-    subplot(1,2,2);
+    subplot(2,2,2);
         plot3(pro2.ReducedModel.XYZ(:,1), pro2.ReducedModel.XYZ(:,2), pro2.ReducedModel.XYZ(:,3) )
         grid on
         title('1AKE in 3D Space')
+        
+    subplot(2,2,3);
+        plot3(pro1.ReversedModel.XYZ(:,1), pro1.ReversedModel.XYZ(:,2), pro1.ReversedModel.XYZ(:,3) )
+        grid on
+        title('Reverse Coordinated 4AKE')
+    subplot(2,2,4);
+        plot3(pro2.ReversedModel.XYZ(:,1), pro2.ReversedModel.XYZ(:,2), pro2.ReversedModel.XYZ(:,3) )
+        grid on
+        title('Reverse Coordinated 1AKE')
         
 % Ramachandran Plots
 figure(2)
 
     subplot(1,2,1);
-        scatter(pro1.PhiPsiAngles(:,1), pro1.PhiPsiAngles(:,2), 15, 'filled', 'k');
+        scatter(pro1.PhiPsiAngles(:,1), pro1.PhiPsiAngles(:,2), 20, 'filled', 'k');
             xticks([-180:45:+180]);
             yticks([-180:45:+180]);
             xlim([-180 +180]);
@@ -35,7 +45,7 @@ figure(2)
             ylabel('Psi (Degrees)');
 
     subplot(1,2,2);
-        scatter(pro2.PhiPsiAngles(:,1), pro2.PhiPsiAngles(:,2), 15, 'filled', 'k');
+        scatter(pro2.PhiPsiAngles(:,1), pro2.PhiPsiAngles(:,2), 20, 'filled', 'k');
             xticks([-180:45:+180]);
             yticks([-180:45:+180]);
             xlim([-180 +180]);
@@ -47,10 +57,10 @@ figure(2)
             ylabel('Psi (Degrees)');
  
 % Plotted onto built-in ramachandran to see if it is correct
-%   ramachandran_official('4AKE.pdb','chain','A') 
-% 	hold on
-% 	scatter(pro1.PhiPsiAngles(:,1), pro1.PhiPsiAngles(:,2))
-%     
+  ramachandran_official('4AKE.pdb','chain','A') 
+	hold on
+	scatter(pro1.PhiPsiAngles(:,1), pro1.PhiPsiAngles(:,2))
+    
 % 	ramachandran_official('1AKE.pdb','chain','A')
 % 	hold on
 % 	scatter(pro2.PhiPsiAngles(:,1), pro2.PhiPsiAngles(:,2))
@@ -61,15 +71,15 @@ figure(3)
     subplot(3,1,1); % Bond length vs. residue index
         plot(pro1.ReducedModel.BondLengths)
             title('Bond Lengths vs. Residue Index')
-            xticks([1:5:213]);
-            xlim([0, 214]);
+            xticks([0:5:215]);
+            xlim([-2, 215]);
                 hold on 
                 plot(pro2.ReducedModel.BondLengths)
                 hold off
     subplot(3,1,2); % Bond angle vs. residue index
         plot(pro1.ReducedModel.BondAngles)
-            xticks([1:5:213]);
-            xlim([0, 214]);
+            xticks([0:5:215]);
+            xlim([-2, 215]);
             title('Bond Angles vs. Residue Index')
                 hold on 
                 plot(pro2.ReducedModel.BondAngles)
@@ -77,8 +87,8 @@ figure(3)
     subplot(3,1,3); % Torsion angle vs. residue index
         plot(pro1.ReducedModel.TorsionalAngles)
             title('Torsional Angles vs. Residue Index')
-            xticks([1:5:211]);
-            xlim([0, 212]);
+            xticks([0:5:215]);
+            xlim([-2, 215]);
                 hold on 
                 plot(pro2.ReducedModel.TorsionalAngles)
                 hold off
